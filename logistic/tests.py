c761054777b574dbeb697da5bd0ee5c01ae5f2ba
Maxim Vasilkin <mxvas@mail.ru>
2023-03-13 21:24:32 +0400

    add main page and test main page

--- logistic/tests.py
+++ logistic/tests.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
+import requests
 
 
-class SomeTest(TestCase):
-    def test_view(self):
-        self.assertEqual(5, 5)
+class CiCd(TestCase):
+    def test_run_server(self):
+        response = requests.get('http://185.182.111.243/')
+        reference = 'Stocks-Products API'
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text, reference)
--- logistic/views.py
+++ logistic/views.py
@@ -1,8 +1,26 @@
+from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
+
+
+def main_page(request):
+    msg = f'Stocks-Products API'
+    page = '''
+    <!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+        <h1>{}</h1>
+    </body>
+    </html>
+    '''.format(msg)
+    return HttpResponse(page)
 
 
 class ProductViewSet(ModelViewSet):
